f0de5a0ff300e9c22bb4bdb898fde12091c978c6
LCA <LCAMod@danielvalle.net>
2024-01-22 20:10:19 -0500

    update

---
+++ bg2/copycommandgenerator.ps1
@@ -0,0 +1,26 @@
+$basePath = "lca/bg2/"
+$outPath = "copyCommands.txt"
+
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+$folders = Get-ChildItem -Directory $scriptPath
+$outString = ""
+$folderName = ""
+foreach($folder in $folders){
+	$files = Get-ChildItem -Recurse $folder
+	if($folder -ne $folderName){
+		if($folderName -ne ""){
+			$outString += "//}"
+			$outString += [Environment]::NewLine
+		}
+		$foldername = $folder
+		$outString += "//{ " + $foldername  
+		$outString += [Environment]::NewLine
+	}
+	
+	foreach($file in $files){
+		$outString += "`t COPY ~" + $basePath + $folder + "/" + $file + "~ ~override~"  
+		$outString += [Environment]::NewLine
+	}
+}
+Out-File -FilePath $outPath -InputObject $outString
