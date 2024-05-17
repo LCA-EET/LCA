296e901ba6cdb66816f97aa5759cd2ca38a0771f
LCA-EET <LCAMod@danielvalle.net>
2024-05-17 14:31:58 -0400

    update

--- EE/rbg/d/xagorion.d
+++ EE/rbg/d/xagorion.d
@@ -84,21 +84,125 @@ END
 IF ~~ THEN BEGIN XA_HasRelationship
 	SAY @36 /* ~That's wonderful! Your foster father would be pleased, child.~ */
 	
+	= @61 /*~If I may... what are they like? What about them attracted you?~*/
+	
+	IF ~~ THEN REPLY @67 /*~I'm afraid that's not any of your business.~*/
+	GOTO XA_WontTell
+	
+	IF ~~ THEN REPLY @68 /*~I'd rather not say.~*/
+	GOTO XA_WontTell
+	
+	IF ~
+		Global("JaheiraRomanceActive", "GLOBAL", 2)
+	~ THEN REPLY @62 /*~Her name is Jaheira. She's strong, caring and resolute.~*/
+	GOTO XA_Indulge
+	
+	IF ~
+		Global("AerieRomanceActive", "GLOBAL", 2)
+	~ THEN REPLY @63 /*~Her name is Aerie. She's sweet, innocent and caring.~*/
+	GOTO XA_Indulge
+	
+	IF ~
+		Global("AnomenRomanceActive", "GLOBAL", 2)
+	~ THEN REPLY @64 /*~~His name is Anomen. He's strong, courageous and resolute.~*/
+	GOTO XA_Indulge
+	
+	IF ~
+		Global("RasaadRomanceActive", "GLOBAL", 2)
+	~ THEN REPLY @65 /*~His name is Rasaad. He's wise, thoughtful and resolute.~*/
+	GOTO XA_Indulge
+	
+	IF ~
+		Global("XA_LC_CorwinRomanceActive", "GLOBAL", 2)
+	~ THEN REPLY @66 /*~Her name is Schael. She's assertive, strong and resolute.~*/
+	GOTO XA_Indulge
+	
+	IF ~
+		Global("NeeraRomanceActive", "GLOBAL", 2)
+	~ THEN REPLY @67 /*~Her name is Neera. She's fiery, creative, and kind.~*/
+	GOTO XA_Indulge
+END
+
+IF ~~ THEN BEGIN XA_WontTell
+	SAY @71 /*~(The simulacrum nods silently.)~*/
+	
+	COPY_TRANS XAGORION XA_MoreQuestions
+END
+
+IF ~~ THEN BEGIN XA_Indulge
+	SAY @70 /* ~Wonderful...~ */
+	
 	IF ~~ THEN
-	GOTO XA_MoreQuestions
+	DO ~
+		IncrementGlobal("XA_LC_GorionQuestions", "GLOBAL", 1)
+		ChangeStat(Player1, XP, 1000, ADD)
+	~
+	GOTO XA_Indulge2
+END
+
+IF ~~ THEN BEGIN XA_Indulge2
+	SAY @72 /*~Thank you, child, for indulging me.~*/
+	
+	COPY_TRANS XAGORION XA_MoreQuestions
 END
 
 IF ~~ THEN BEGIN XA_NoRelationship
 	SAY @35 /* ~That's unfortunate, though not surprising. Given the dangers you face, it's no wonder that you're reluctant to engage in such relationships.~ */
 	
-	IF ~~ THEN
-	GOTO XA_MoreQuestions
+	COPY_TRANS XAGORION XA_MoreQuestions
 END
 
 IF ~~ THEN BEGIN XA_Prepare
-	SAY @8 /* ~Ah... I'd hoped that Alaundo was wrong. I thought that sheltering you from the outside world would spare you from all of the death and bloodshed that was ultimately thrust upon you. Forgive me.~ */	
-	IF ~~ THEN
-	GOTO XA_MoreQuestions
+	SAY @8 /* ~Ah... I'd hoped that Alaundo was wrong. I thought that sheltering you from the outside world would spare you from all of the death and bloodshed that was ultimately thrust upon you.~ */	
+	
+	= @73 /* ~Would it have made a difference, I wonder... Would you have been spared from having to slay your half-brother, or from venturing to Avernus?~ */
+	
+	= @74 /*  ~Would better preparation in Candlekeep have prevented your capture by Irenicus? I think not... but what do you think, child?~ */
+	
+	IF ~~ THEN REPLY @75/* ~I agree. No amount of preparation would've made a difference.~*/
+	GOTO XA_WontTell
+	
+	IF ~~ THEN REPLY @76/* ~Had I spent more time studying in the library, I may have gained the intellect to outsmart my captor and escape.~*/
+	DO ~
+		SetGlobal("XA_LC_StatBoost", "LOCALS", 1) // int
+	~
+	GOTO XA_Forgive
+	
+	IF ~~ THEN REPLY @77/* ~Spending more time speaking with you, Tethroril, and the others may have have afforded me the wisdom and insight to see what Irenicus had planned for me.~*/
+	DO ~
+		SetGlobal("XA_LC_StatBoost", "LOCALS", 2) // wis
+	~
+	GOTO XA_Forgive
+	
+	IF ~~ THEN REPLY @78/* ~I could've grown stronger had I spent more time training with the Watchers. The increase in strength could have helped me to break my bonds and escape.~*/
+	DO ~
+		SetGlobal("XA_LC_StatBoost", "LOCALS", 3) // str
+	~
+	GOTO XA_Forgive
+	
+	IF ~~ THEN REPLY @79/* ~More time spent with Winthrop and his patrons could've better developed my social skills, making me into a more charismatic individual. Were it so, I may have been able to convince the Dukes and the city of my innocence, and in doing so prevent my exile and subsequent capture.~*/
+	DO ~
+		SetGlobal("XA_LC_StatBoost", "LOCALS", 4) // cha
+	~
+	GOTO XA_Forgive
+	
+	IF ~~ THEN REPLY @80/* ~Working in the infirmary would've taught my body how to fight off illness and disease. The increase in my constitution could've helped me to resist the poisons that were used to subdue me.~ */
+	DO ~
+		SetGlobal("XA_LC_StatBoost", "LOCALS", 5) // con
+	~
+	GOTO XA_Forgive
+	
+	IF ~~ THEN REPLY @81/* ~The Gatewarden could have taught me much concerning weapons and combat. Were my skill and dexterity greater, I may have been able to fend off the assassins sent to capture me.~*/
+	DO ~
+		SetGlobal("XA_LC_StatBoost", "LOCALS", 6) // dex
+	~
+	GOTO XA_Forgive
+END
+
+IF ~~ THEN BEGIN XA_Forgive
+	SAY @82 /* ~I see... perhaps you are right. Forgive me.~*/
+	
+	COPY_TRANS XAGORION XA_MoreQuestions
 END
 
 IF ~~ THEN BEGIN XA_MoreQuestions
@@ -118,7 +222,7 @@ IF ~~ THEN BEGIN XA_MoreQuestions
 	GOTO XA_Leave3
 	
 	IF ~
-		!Global("XA_LC_Prepare", "LOCALS", 1)
+		GlobalLT("XA_LC_Prepare", "LOCALS", 1)
 	~ THEN REPLY @6 /* ~Why didn't you prepare Imoen and I for what was coming?~*/
 	DO ~
 		SetGlobal("XA_LC_Prepare", "LOCALS", 1)
@@ -128,7 +232,7 @@ IF ~~ THEN BEGIN XA_MoreQuestions
 	GOTO XA_Prepare
 	
 	IF ~
-		!Global("XA_LC_Prepare", "LOCALS", 1)
+		GlobalLT("XA_LC_Prepare", "LOCALS", 1)
 	~ THEN REPLY @7 /* ~You've always known what Imoen and I were. Why didn't you tell us?~ */
 	DO ~
 		SetGlobal("XA_LC_Prepare", "LOCALS", 1)
@@ -138,7 +242,7 @@ IF ~~ THEN BEGIN XA_MoreQuestions
 	GOTO XA_Prepare
 	
 	IF ~
-		!Global("XA_LC_Mother", "LOCALS", 1)
+		GlobalLT("XA_LC_Mother", "LOCALS", 1)
 	~ THEN REPLY @29 /* ~What can you tell me about my mother?~ */
 	DO ~
 		SetGlobal("XA_LC_Mother", "LOCALS", 1)
@@ -148,7 +252,7 @@ IF ~~ THEN BEGIN XA_MoreQuestions
 	GOTO XA_Mother
 	
 	IF ~
-		!Global("XA_LC_Escape", "LOCALS", 1)
+		GlobalLT("XA_LC_Escape", "LOCALS", 1)
 	~ THEN REPLY @38 /* ~How do I escape this prison?~ */
 	DO ~
 		SetGlobal("XA_LC_Escape", "LOCALS", 1)
@@ -158,7 +262,7 @@ IF ~~ THEN BEGIN XA_MoreQuestions
 	GOTO XA_Escape
 	
 	IF ~
-		!Global("XA_LC_WhatWillHappen", "LOCALS", 1)
+		GlobalLT("XA_LC_WhatWillHappen", "LOCALS", 1)
 		GlobalGT("XA_LC_GorionQuestions", "GLOBAL", 2)
 	~ THEN REPLY @12 /* ~What will happen to you when I leave here?~*/
 	DO ~
@@ -169,7 +273,7 @@ IF ~~ THEN BEGIN XA_MoreQuestions
 	GOTO XA_WhatWillHappen
 	
 	IF ~
-		!Global("XA_LC_GorionImpression", "LOCALS", 1)
+		GlobalLT("XA_LC_GorionImpression", "LOCALS", 1)
 		GlobalGT("XA_LC_GorionQuestions", "GLOBAL", 3)
 	~ THEN REPLY @14 /* ~What would Gorion think of the <PRO_MANWOMAN> I've become?~ */
 	DO ~
@@ -180,7 +284,7 @@ IF ~~ THEN BEGIN XA_MoreQuestions
 	GOTO XA_GorionImpression
 	
 	IF ~
-		!Global("XA_LC_WhyHarpers", "LOCALS", 1)
+		GlobalLT("XA_LC_WhyHarpers", "LOCALS", 1)
 	~ THEN REPLY @55 /* ~Why join the Harpers?~ */
 	DO ~
 		SetGlobal("XA_LC_WhyHarpers", "LOCALS", 1)
@@ -198,8 +302,7 @@ IF ~~ THEN BEGIN XA_WhyHarpers
 	= @58 /* ~You've met many of them already. I hope, <CHARNAME>, that you are able to maintain the friendships and relationships with your companions, as I did with mine. Doing so will benefit you and your comrades in ways that are not always easy to see.~
  */
  
-	IF ~~ THEN
-	GOTO XA_MoreQuestions
+	COPY_TRANS XAGORION XA_MoreQuestions
 END
 
 IF ~~ THEN BEGIN XA_GorionImpression
@@ -255,8 +358,15 @@ END
 IF ~~ THEN BEGIN XA_GorionImpression_VeryGood_M
 	SAY @49 /* ~You've always strived to do what is right, and your many heroic deeds speak for themselves. He'd be proud... no... he'd be honored to have played a role in your upbringing, young man.~ */
 	
-	IF ~~ THEN 
+	IF ~
+		GlobalLT("XA_LC_GorionQuestions", "GLOBAL", 5)
+	~ THEN 
 	GOTO XA_Leave2
+	
+	IF ~
+		GlobalGT("XA_LC_GorionQuestions", "GLOBAL", 4)
+	~ THEN 
+	GOTO XA_Leave4
 END
 
 IF ~~ THEN BEGIN XA_GorionImpression_VeryGood_F
@@ -288,7 +398,7 @@ IF ~~ THEN BEGIN XA_GorionImpression_Bad
 END
 
 IF ~~ THEN BEGIN XA_GorionImpression_VeryBad
-	SAY @17 /* ~He would be disgusted and filled with regret. You've discarded his teachings entirely, and have terrorized the people of Amn and the Sword Coast. He would do everything in his power to stop you.~ */
+	SAY @17 /* ~He would be disgusted and filled with regret. You've discarded his teachings entirely, and have terrorized the people of Amn and the Sword Coast. He would do everything in his power to stop you. And so would I~ */
 	
 	IF ~~ THEN
 	DO ~
@@ -314,11 +424,10 @@ IF ~~ THEN BEGIN XA_Escape
 	
 	= @42 /* ~Such knowledge could prove invaluable if you choose to confront the master in his sanctum.~ */
 	
-	IF ~~ THEN REPLY @43 /* I see... */
-	GOTO XA_MoreQuestions
-	
 	IF ~~ THEN REPLY @44 /* ~Why are you helping me?~ */
 	GOTO XA_WhyHelp
+	
+	COPY_TRANS XAGORION XA_MoreQuestions
 END
 
 IF ~~ THEN BEGIN XA_WhyHelp
@@ -346,4 +455,66 @@ IF ~~ THEN BEGIN XA_Leave3
 		SetGlobal("XA_LC_EndG3", "GLOBAL", 1)
 	~
 	EXIT	
+END
+
+IF ~~ THEN BEGIN XA_Leave4
+	SAY @84 /*~(He places his hand on your shoulder and closes his eyes. For a brief moment, your mind is filled with memories of events in Candlekeep that you're certain never took place... a strange sensation, to say the least.)~ */
+	
+	IF ~
+		Global("XA_LC_StatBoost", "LOCALS", 1)
+	~
+	THEN DO ~
+		ChangeStat(Player1, INT, 1, ADD)
+	~
+	GOTO XA_Leave4A
+	
+	IF ~
+		Global("XA_LC_StatBoost", "LOCALS", 2)
+	~
+	THEN DO ~
+		ChangeStat(Player1, WIS, 1, ADD)
+	~
+	GOTO XA_Leave4A
+	
+	IF ~
+		Global("XA_LC_StatBoost", "LOCALS", 3)
+	~
+	THEN DO ~
+		ChangeStat(Player1, STR, 1, ADD)
+	~
+	GOTO XA_Leave4A
+	
+	IF ~
+		Global("XA_LC_StatBoost", "LOCALS", 4)
+	~
+	THEN DO ~
+		ChangeStat(Player1, CHR, 1, ADD)
+	~
+	GOTO XA_Leave4A
+	
+	IF ~
+		Global("XA_LC_StatBoost", "LOCALS", 5)
+	~
+	THEN DO ~
+		ChangeStat(Player1, CON, 1, ADD)
+	~
+	GOTO XA_Leave4A
+	
+	IF ~
+		Global("XA_LC_StatBoost", "LOCALS", 6)
+	~
+	THEN DO ~
+		ChangeStat(Player1, DEX, 1, ADD)
+	~
+	GOTO XA_Leave4A
+END
+
+IF ~~ THEN BEGIN XA_Leave4A
+	SAY @85 /* ~Now, one final bit of advice. Though you defeated Sarevok, Aluando's prophecy spoke of scores of Bhaalspawn sowing chaos throughout the land. It's likely the case that fate has more in store for you. Remember that you must remain vigilant at all times, child. Good luck.~  */
+	
+	IF ~~ THEN
+	DO ~
+		SetGlobal("XA_LC_EndG3", "GLOBAL", 1)
+	~
+	EXIT
 END
