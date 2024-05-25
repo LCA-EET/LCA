16a16b6c522fd80460b65fc2627bbcb86c5cae60
practikit <LCAMod@danielvalle.net>
2024-05-25 12:35:44 -0400

    update

---
+++ EE/bg1/d/xacoras.d
@@ -0,0 +1,28 @@
+BEGIN ~xacoras0~
+
+	IF ~
+		NumTimesTalkedTo(0)
+	~ THEN BEGIN XA_Intro
+		SAY @0 /**/
+		
+		IF ~~ THEN
+		DO ~
+			Enemy()
+		~
+		EXTERN xacoras1 XA_End
+	END
+
+END
+
+BEGIN ~xacoras1~
+	IF ~~ THEN BEGIN XA_End
+		SAY @1 /* ~Kill them!~ */
+
+		IF ~~ THEN
+		DO ~
+			Enemy()
+		~
+		EXIT
+	END
+
+END
