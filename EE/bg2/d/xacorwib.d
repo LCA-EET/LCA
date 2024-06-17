86c99fa266d085d16beb304753e2309d6271cf4c
practikit <LCAMod@danielvalle.net>
2024-06-17 10:39:08 -0400

    update

--- EE/bg2/d/xacorwib.d
+++ EE/bg2/d/xacorwib.d
@@ -1523,6 +1523,131 @@ IF ~~ THEN BEGIN XA_OPT1RomanceEnd
 END
 //} #endregion
 
+
+//{ BMAZZY
+APPEND BMAZZY
+	IF ~~ THEN BEGIN BCorwinMazzy2_BREAK
+		SAY @1006 /* ~It's alright. I know you didn't mean anything by it. Besides, I'm quite happy with the crossbow.~*/
+		
+		IF ~
+			GlobalGT("XA_LC_ReturnToBG", "GLOBAL", 0)
+		~ THEN GOTO BCorwinMazzy2_RBG
+		
+		IF ~
+			GlobalLT("XA_LC_ReturnToBG", "GLOBAL", 1)
+		~ THEN GOTO BCorwinMazzy2_RESUME		
+	END
+	
+	IF ~~ THEN BEGIN BCorwinMazzy2_RBG
+		SAY @1007 /* ~Looks like the others are ready... let's get moving.~ */
+		
+		IF ~~ THEN 
+		EXIT
+	END
+	
+	IF ~~ THEN BEGIN XA_MazzyWonCompetition
+		SAY @1008 /* ~Thank you, Schael. You're one of the toughest competitors I've faced.~ */
+		
+		IF ~~ THEN REPLY @1009 /* ~You both performed well. I'm glad to have such skilled companions at my side.~  */
+		GOTO XA_MazzyWonCompetition2
+	END
+	
+	IF ~
+		Global("XA_LC_TM_ArcheryChallenge", "GLOBAL", 2)
+		Global("XA_LC_CorwinRomanceActive", "GLOBAL", 2)
+	~ THEN BEGIN XA_Trademeet_Competition_SchaelWon
+		SAY @1010 /* ~Schael, congratulations on your victory. That was quite the performance.~ */
+	
+		IF ~~ THEN 
+		EXTERN XACORWIB XA_CorwinWonCompetition
+	END
+END
+//}
+
+//{ BDORN
+APPEND BDORN
+	IF ~~ THEN BEGIN XA_CorwinDornConflict2
+		SAY @1011 /* ~It is not your concern.~ */
+		
+		IF ~~ THEN 
+		EXTERN XACORWIB XA_CorwinDornConflict3
+	END
+	
+	IF ~~ THEN BEGIN XA_CorwinDornConflict4
+		SAY @1012 /* ~Laughable, woman. Stand in my way, and your neck will snap as easily as a tree in the midst of a hurricane.~ */
+		
+		IF ~~ THEN REPLY @1013 /* ~That's enough. Dorn, I don't know what you think exists between us, but I love Schael and nothing is going to change that.~  */
+		DO ~
+			SetGlobal("XA_LC_CorwinDornConflict", "GLOBAL", 2)
+		~
+		GOTO XA_ChooseCorwin
+		
+		IF ~~ THEN REPLY @1014 /* ~Stop it, Dorn. You know my affections lie with you.~ */
+		DO ~
+			SetGlobal("XA_LC_CorwinDornConflict", "GLOBAL", 2)
+		~
+		EXTERN XACORWIB XA_ChooseDorn
+	END
+	
+	IF ~~ THEN BEGIN XA_ChooseCorwin
+		SAY @1015 /* ~Bah! To hell with the lot of you!~ */
+		
+		IF ~~ THEN
+		DO ~
+			SetGlobal("OHD_corwinconflict","GLOBAL",4)
+			SetGlobal("DornRomanceActive", "GLOBAL", 3)
+		~
+		EXIT
+	END
+END
+//}
+
+
+//{ BANOMEN
+APPEND ~BANOMEN~
+
+IF ~~ THEN BEGIN BCorwinAnomen1A
+		SAY @1016 /* ~A mother's place is at home with her child, not on the battlefield.~ */
+	
+		= @1017 /* ~You're a poor example of a parent.~ */
+		
+		IF ~~ THEN REPLY @1018 /* ~You're out of line Anomen. Knock it off.~ */
+		EXTERN XACORWIB BCorwinAnomen1B
+		
+		IF ~~ THEN REPLY @1019 /* ~(Remain silent)~ */
+		EXTERN XACORWIB BCorwinAnomen1B
+		
+		IF ~
+			!Global("XA_LC_CorwinPromoted", "GLOBAL", 1)
+			!Global("XA_LC_CorwinRomanceActive", "GLOBAL", 2)
+		~ THEN REPLY @1020 /* ~You know little of what you speak. Captain Corwin is a fine mother.~ */
+		DO ~
+			IncrementGlobal("XA_LC_CorwinOpinionOfPlayer", "GLOBAL", 1)
+		~
+		EXTERN XACORWIB BCorwinAnomen1B
+		
+		IF ~
+			Global("XA_LC_CorwinPromoted", "GLOBAL", 1)
+			!Global("XA_LC_CorwinRomanceActive", "GLOBAL", 2)
+		~ THEN REPLY @1021 /* ~You know little of what you speak. Major Corwin is a fine mother.~ */
+		DO ~
+			IncrementGlobal("XA_LC_CorwinOpinionOfPlayer", "GLOBAL", 1)
+		~
+		EXTERN XACORWIB BCorwinAnomen1B
+		
+		IF ~
+			Global("XA_LC_CorwinRomanceActive", "GLOBAL", 2)
+		~ THEN REPLY @1022 /* ~You know little of what you speak. Schael is a fine mother.~ */
+		DO ~
+			IncrementGlobal("XA_LC_CorwinOpinionOfPlayer", "GLOBAL", 1)
+		~
+		EXTERN XACORWIB BCorwinAnomen1B
+		
+	END
+END
+//}
+
+
 //{ Corwin Sick Dialogs
 
 	//{ Dialog B-5
