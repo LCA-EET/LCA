a0b68f2be3ebaa05f8a717cb4e620f46b6916494
practikit <LCAMod@danielvalle.net>
2024-06-30 21:04:06 -0400

    update

--- EE/bg2/compat/CGS/D/xacgs.d
+++
@@ -1,9 +0,0 @@
-/* This change will only be made if the Cost of One Girl's Soul Mod is not installed */
-
-ADD_TRANS_ACTION IDJINNI
-BEGIN 12 END
-BEGIN 0 END
-~
-	GiveItemCreate("XASTDAG",LastTalkedToBy(Myself),0,0,0)
-	AddJournalEntry(@0, INFO)
-~
