1d9eea4317a2d197a66e61554de498b79075cf4f
LCA-EET <LCAMod@danielvalle.net>
2024-07-20 21:28:35 -0400

    update

--- EE/rbg/d/xafergus.d
+++ EE/rbg/d/xafergus.d
@@ -26,7 +26,7 @@ END
 
 IF ~
 	!Global("XA_LC_FergusMarried", "GLOBAL", 1)
-	Global("XA_LC_BrielbaraCoran", "GLOBAL", 1)
+	Global("XA_LC_FergusRing", "GLOBAL", 1)
 	!IsGabber(Player1)
 ~ THEN BEGIN XA_FergusCharname
 	SAY @10 /* ~Is <CHARNAME> with you? I'd like to speak with <PRO_HIMHER>, if <PRO_HESHE>'s available.~ */
@@ -34,7 +34,7 @@ IF ~
 END
 
 IF ~
-	Global("XA_LC_BrielbaraCoran", "GLOBAL", 1)
+	Global("XA_LC_FergusRing", "GLOBAL", 1)
 	IsGabber(Player1)
 	!Global("XA_LC_FergusMarried", "GLOBAL", 1)
 ~ THEN BEGIN XA_FergusMarried
@@ -59,7 +59,7 @@ IF ~
 	//Global("XA_LC_FergusMarried", "GLOBAL", 1)
 	RandomNum(2,2)
 ~ THEN BEGIN XAA1
-	SAY @8 /* ~Nothing unusual to report, Captain.~ */
+	SAY @16 /* ~If you're looking for Marshal Nederlok, he is in his office upstairs.~ */
 	IF ~~ THEN EXIT
 END
 
@@ -68,13 +68,22 @@ IF ~~ THEN BEGIN XA_FergusMarriedA
 	
 	IF ~~ THEN REPLY @4 /* ~Oh, that's great news. Congratulations to you and your wife!~ */
 	GOTO XA_FergusMarriedC
+	
+	IF ~
+		Gender(Player1, MALE)
+	~ THEN REPLY @17 /*~Glad to hear. Have you decided on a name for the baby?~*/
+	GOTO XA_BabyNameBoy
+	
+	IF ~
+		Gender(Player1, FEMALE)
+	~ THEN REPLY @17 /*~Glad to hear. Have you decided on a name for the baby?~*/
+	GOTO XA_BabyNameGirl
 END
 
 IF ~~ THEN BEGIN XA_FergusMarriedB
 	SAY @3 /* ~Yes, you remembered! I wanted to thank you again. My wife loved the angel skin ring, and we're expecting our first baby in a few months' time.~ */
 	
-	IF ~~ THEN REPLY @4 /* ~Oh, that's great news. Congratulations to you and your wife!~  */
-	GOTO XA_FergusMarriedC
+	COPY_TRANS XAFERGUS XA_FergusMarriedA
 END
 
 IF ~~ THEN BEGIN XA_FergusMarriedC
@@ -87,4 +96,17 @@ IF ~~ THEN BEGIN XA_FergusMarriedC
 		SetGlobal("XA_LC_FergusMarried", "GLOBAL", 1)
 	~
 	EXIT
+END
+
+IF ~~ THEN BEGIN XA_BabyNameBoy
+	SAY @18 /*~Not yet, but we've narrowed it down to five or six choices... if it's a boy, we may name him <CHARNAME>.~*/
+	
+	IF ~~ THEN REPLY @20 /*~I'm honored. Good luck with the baby.~*/
+	GOTO XA_FergusMarriedC
+END
+
+IF ~~ THEN BEGIN XA_BabyNameGirl
+	SAY @19 /*~Not yet, but we've narrowed it down to five or six choices... if it's a girl, we may name her <CHARNAME>.~*/
+	
+	COPY_TRANS XAFERGUS XA_BabyNameBoy
 END
