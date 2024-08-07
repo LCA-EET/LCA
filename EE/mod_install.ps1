8ccd47ed178bccfdc00bdc8b9c6b611e96a493d1
LCA-EET <129810393+LCA-EET@users.noreply.github.com>
2024-08-06 22:42:56 -0400

    update

---
+++ EE/mod_install.ps1
@@ -0,0 +1,4 @@
+./setup-LCA.exe --yes
+./setup-LCA_Explicit.exe --yes --args 1
+./setup-AutoIdentify.exe --yes
+./setup-AutoLoot.exe --yes
---
+++ EE/mod_uninstall.ps1
@@ -0,0 +1,6 @@
+./setup-AutoLoot.exe --uninstall
+./setup-AutoIdentify.exe --uninstall
+./setup-LCA_Explicit.exe --uninstall
+./setup-LCA.exe --uninstall
+
+
