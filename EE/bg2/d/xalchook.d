e95302207a11217ea12cc8515cbcb48ef2f39c2c
LCA-EET <129810393+LCA-EET@users.noreply.github.com>
2025-01-25 22:28:49 -0500

    update

--- EE/bg2/d/xalchook.d
+++ EE/bg2/d/xalchook.d
@@ -6,6 +6,7 @@ IF ~
 ~ THEN BEGIN XA_SleepInn
 	SAY @2  /* ~(You and Schael eagerly step into the room, and make your way to the bed...)~*/
 	
+	// 0
 	IF ~
 		Gender(Player1, FEMALE)
 		Global("XA_LC_CorwinSex_1", "GLOBAL", 1)
@@ -16,6 +17,7 @@ IF ~
 	~
 	EXIT
 	
+	// 1
 	IF ~
 		Gender(Player1, FEMALE)
 		Global("XA_LC_CorwinSex_2", "GLOBAL", 1)
@@ -27,13 +29,29 @@ IF ~
 	~
 	EXIT
 	
+	// 2
 	IF ~
 		Gender(Player1, MALE)
-		OR(2)
-			Global("XA_LC_CorwinSex_1", "GLOBAL", 1)
-			Global("XA_LC_CorwinSex_2", "GLOBAL", 1)
+		Global("XA_LC_CorwinSex_1", "GLOBAL", 1)
 	~
-	THEN GOTO XA_NeedExplicitMod
+	THEN 
+	DO ~
+		StartCutSceneMode()
+		StartCutScene("XAEX01EX")
+	~
+	EXIT
+	
+	// 3
+	IF ~
+		Gender(Player1, MALE)
+		Global("XA_LC_CorwinSex_2", "GLOBAL", 1)
+	~
+	THEN 
+	DO ~
+		StartCutSceneMode()
+		StartCutScene("XAEX02EX")
+	~
+	EXIT
 END
 //}
 
@@ -45,65 +63,12 @@ IF ~
 ~ THEN BEGIN XA_SleepBG
 	SAY @0  /* ~(You make your way to your room and get comfortable while you wait for Schael to arrive...)~ */
 	
-	IF ~
-		Global("XA_LC_CorwinSex_3", "GLOBAL", 1)
-		Gender(Player1, FEMALE)
-	~
-	THEN
-	DO ~
-		SetGlobal("XA_LC_SleepBG", "GLOBAL", 0)
-		StartCutSceneMode()
-		StartCutScene("XAEX03EX")
-	~
-	EXIT
-	
-	IF ~
-		Global("XA_LC_CorwinSex_3", "GLOBAL", 1)
-		Gender(Player1, MALE)
-	~
-	THEN GOTO XA_NeedExplicitMod
-END
-
-//}
-
-//{ Explicit Mod Needed
-IF ~~ THEN BEGIN XA_NeedExplicitMod
-	SAY @1  /* ~(Install the LCA_Explicit mod to experience this content.)~*/
-	
-	IF ~~ THEN
-	DO ~
-		SetGlobal("XA_LC_SleepBG", "GLOBAL", 0)
-		SetGlobal("XA_LC_SleepInn", "GLOBAL", 0)
-	~
-	EXIT
-	
-	IF ~
-		Global("XA_LC_CorwinSex_1", "GLOBAL", 1)
-	~ THEN
-	DO ~
-		StartCutSceneMode()
-		StartCutScene("XAEX01EX")
-	~
-	EXIT
-	
-	IF ~
-		Global("XA_LC_CorwinSex_2", "GLOBAL", 1)
-	~
-	THEN
-	DO ~
-		StartCutSceneMode()
-		StartCutScene("XAEX02EX")
-	~
-	EXIT
-	
-	IF ~
-		Global("XA_LC_CorwinSex_3", "GLOBAL", 1)
-	~
-	THEN
+	IF ~~ THEN 
 	DO ~
 		StartCutSceneMode()
 		StartCutScene("XAEX03EX")
 	~
 	EXIT
 END
+
 //}
