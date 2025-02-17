a4dc78b960b6a458172b6d33927d9d12022ea31f
LCA-EET <LCAMod@danielvalle.net>
2025-02-17 11:08:38 -0500

    update

--- EE/makepkg_combined.ps1
+++ EE/makepkg_combined.ps1
@@ -48,13 +48,13 @@ foreach($folder in $toExclude){
 Copy-Item -Path ("functions.tph") -Destination $modPath 
 Copy-Item -Path ($tp2Name + ".tp2") -Destination $modPath 
 Copy-Item -Path "weidu.exe" -Destination ($basePath + "/" + $exePath)
-Copy-Item -Path "Discord Server.url" -Destination ($basePath + "/Discord Server.url")
-Copy-Item -Path "PayPal.url" -Destination ($basePath + "/PayPal.url")
-Copy-Item -Path "LCA User Guide.url" -Destination ($basePath + "/LCA User Guide.url")
-Copy-Item -Path "G3 Forum Post.url" -Destination ($basePath + "/G3 Forum Post.url")
-Copy-Item -Path "Venmo.url" -Destination ($basePath + "/Venmo.url")
-Copy-Item -Path "Beamdog Forum Post.url" -Destination ($basePath + "/Beamdog Forum Post.url")
-Copy-Item -Path "LCA Release Notes.url" -Destination ($basePath + "/LCA Release Notes.url")
+Copy-Item -Path "Discord Server.url" -Destination $modPath
+Copy-Item -Path "PayPal.url" -Destination $modPath
+Copy-Item -Path "LCA User Guide.url" -Destination $modPath
+Copy-Item -Path "G3 Forum Post.url" -Destination $modPath
+Copy-Item -Path "Venmo.url" -Destination $modPath
+Copy-Item -Path "Beamdog Forum Post.url" -Destination $modPath
+Copy-Item -Path "LCA Release Notes.url" -Destination $modPath
 #Copy-Item -Path "Release Notes.md" -Destination ($basePath + "/Release_Notes_LCA.md")
 #Copy-Item -Path "../User Guide.pdf" -Destination ($basePath + "/User Guide.pdf")
 #Remove-Item -LiteralPath ($testDir + $tp2Name) -Force -Recurse
